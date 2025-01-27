136c9e5fc53a18dd417ad067249fcd3baf51afa9
eyuale <157725293+e-lemma@users.noreply.github.com>
2025-01-27 23:10:35 +0000

    Complete exercises 04-07

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+/*
+determine the smaller of the two
+start from that int, increment up
+adding each number to a sum var
+until bigger of two reached inclusive
+then return sum var
+*/
 
+const sumAll = function (int1, int2) {
+  for (const int of [int1, int2]) {
+    if (int < 0 || !Number.isInteger(int)) {
+      return "ERROR";
+    }
+  }
+  let startInt = Math.min(int1, int2);
+  let endInt = Math.max(int1, int2);
+  let sum = 0;
+  for (startInt; startInt <= endInt; startInt++) {
+    sum += startInt;
+  }
+  return sum;
 };
 
 // Do not edit below this line
