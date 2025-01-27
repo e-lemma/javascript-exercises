2fae66815f5a71261a5dd2df3da8749280eeeed3
eyuale <157725293+e-lemma@users.noreply.github.com>
2025-01-27 21:54:07 +0000

    Complete exercises 01-03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let repeated = "";
+  if (num < 0) {
+    return "ERROR";
+  }
+  for (num; num > 0; num--) {
+    repeated += string;
+  }
+  return repeated;
 };
 
 // Do not edit below this line
