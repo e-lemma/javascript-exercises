ff3bee9d0cd3913cd7e4a075391e371d82bd1277
eyuale <157725293+e-lemma@users.noreply.github.com>
2025-02-01 15:52:34 +0000

    Complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function (desiredMemberPosition) {
+  const fibSeq = [0];
+  if (desiredMemberPosition < 0) {
+    return "OOPS";
+  }
+  while (fibSeq.length - 1 < desiredMemberPosition) {
+    if (fibSeq.length < 3) {
+      let nextNum = fibSeq.reduce((sum, num) => sum + num, 1);
+      fibSeq.push(nextNum);
+    }
+    nextNum = fibSeq.slice(-2).reduce((sum, num) => sum + num);
+    fibSeq.push(nextNum);
+  }
+  return fibSeq.pop();
 };
+
+console.log(fibonacci(2));
 
 // Do not edit below this line
 module.exports = fibonacci;
