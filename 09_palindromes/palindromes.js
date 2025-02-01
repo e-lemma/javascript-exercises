3f6c869f720a0cbe0e3f2875a28b771a6306966a
eyuale <157725293+e-lemma@users.noreply.github.com>
2025-02-01 14:45:28 +0000

    Complete 08 and 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const disregard = [" ", "!", ",", "."];
 
+  const cleaned = string
+    .toLowerCase()
+    .split("")
+    .filter((char) => !disregard.includes(char))
+    .join("");
+
+  const reversed = cleaned.split("").reverse().join("");
+
+  return cleaned === reversed;
 };
 
 // Do not edit below this line
