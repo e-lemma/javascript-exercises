31fc16e4082b781917961fd8e813fcc886fb68ba
eyuale <157725293+e-lemma@users.noreply.github.com>
2025-02-01 16:29:44 +0000

    Complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+  const currentYear = new Date().getFullYear();
 
+  let oldestName = "";
+  let oldestAge = 0;
+
+  array.forEach((person) => {
+    if (person.yearOfDeath) {
+      currentAge = person.yearOfDeath - person.yearOfBirth;
+    } else {
+      currentAge = currentYear - person.yearOfBirth;
+    }
+
+    if (currentAge > oldestAge) {
+      oldestAge = currentAge;
+      oldestName = person.name;
+    }
+  });
+  return array.find(({ name }) => name === oldestName);
 };
 
 // Do not edit below this line
